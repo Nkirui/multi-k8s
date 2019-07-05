8570496f549d4c0780f46636347904e8f55f2147
boyo <boyo@jitu.mshome>
2019-07-05 17:33:37 +0300

    travis and deploy edits

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ docker push nkirui2030/multi-worker:latest
 docker push nkirui2030/multi-client:$SHA
 docker push nkirui2030/multi-server:$SHA
 docker push nkirui2030/multi-worker:$SHA
-kubctl apply -f k8s
-kubctl set image deployements/server-deployement server=nkirui2030/multi-server:$SHA
-kubctl set image deployements/client-deployement client=nkirui2030/multi-client:$SHA
-kubctl set image deployements/worker-deployement worker=nkirui2030/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployements/server-deployement server=nkirui2030/multi-server:$SHA
+kubectl set image deployements/client-deployement client=nkirui2030/multi-client:$SHA
+kubectl set image deployements/worker-deployement worker=nkirui2030/multi-worker:$SHA
