5020cd56c991b6fe93747eae438caf19724ff36c
boyo <boyo@jitu.mshome>
2019-07-22 00:19:55 +0300

    server edits

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ docker push nkirui2030/multi-client:$SHA
 docker push nkirui2030/multi-server:$SHA
 docker push nkirui2030/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployements/server-deployement server=nkirui2030/multi-server:$SHA
-kubectl set image deployements/client-deployement client=nkirui2030/multi-client:$SHA
-kubectl set image deployements/worker-deployement worker=nkirui2030/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=nkirui2030/multi-server:$SHA
+kubectl set image deployments/client-deployment client=nkirui2030/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=nkirui2030/multi-worker:$SHA
